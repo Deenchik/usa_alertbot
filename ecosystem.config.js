38b4c4b2160a4550603781dd1ea075b93d83af30
Denis Sundukov <shipoklyk@mail.ru>
2025-02-20 06:16:34 +0700

    Add files via upload

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,7 +2,7 @@ module.exports = {
   apps: [{
     name: "usa_alertbot",   // Имя процесса
     script: "./usa_alertbot.py", // Путь к вашему скрипту
-    interpreter: "python3",  // Убедитесь, что используете правильный интерпретатор Python
+    interpreter: "/usr/bin/python3",  // Убедитесь, что используете правильный интерпретатор Python
     instances: 1,            // Запуск только одного экземпляра
     autorestart: true,       // Автоперезапуск
     watch: false,            // Отключаем слежение за изменениями файлов
